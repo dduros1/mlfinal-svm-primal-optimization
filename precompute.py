5f3edc463b428c3fcec2dc5d671ac2bfdbac1e69
thunderkatz <jonathan.d.katzman@gmail.com>
2014-05-08 13:28:11 -0400

    Minor bug fix to precompute

--- precompute.py
+++ precompute.py
@@ -2,13 +2,14 @@ import optimizer
 import pickle
 import time
 from dataparser import *
+from optimizer import *
 
 start = time.time()
 reader = DataReader('data/train.tsv', punct=1)
 reader.readInput()
 data = reader.getData()
 tester = NewtonApproximation(RBF())
-kernel = tester.computekernel(data)
+kernel = tester.compute_kernel(data)
 picklefile = open('precomputedkernel.pkl', 'rw')
 pickle.dump(picklefile, kernel)
 end = time.time()
