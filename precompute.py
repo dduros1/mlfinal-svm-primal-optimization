3648f2fc4a51bdc7ba7e4e5422c5736aa85a41d1
Diane Duros <d@Alan.local>
2014-05-08 13:12:58 -0400

    fixed a few syntax things in precompute.py, think i got the matrix setup figured out with numpy in optimizer

--- precompute.py
+++ precompute.py
@@ -1,14 +1,16 @@
-import optimizer
+#!/usr/bin/python
+
+from optimizer import *
 import pickle
 import time
 from dataparser import *
 
 start = time.time()
-reader = DataReader('data/train.tsv', punct=1)
+reader = DataReader('data/smalltrain.tsv', punct=1)
 reader.readInput()
 data = reader.getData()
 tester = NewtonApproximation(RBF())
-kernel = tester.computekernel(data)
+kernel = tester.compute_kernel(data)
 picklefile = open('precomputedkernel.pkl', 'rw')
 pickle.dump(picklefile, kernel)
 end = time.time()
