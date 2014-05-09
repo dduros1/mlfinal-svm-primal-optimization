b7b8c6f2ce0394344f6a14e770589ba9f9f30087
Jonathan Katzman <Jonathan.D.Katzman@gmail.com>
2014-05-09 11:04:34 -0400

    Slightly different approach to precompute

--- precompute.py
+++ precompute.py
@@ -7,12 +7,18 @@ from dataparser import *
 from optimizer import *
 
 start = time.time()
-reader = DataReader('data/smalltrain.tsv', punct=1)
+reader = DataReader('data/train.tsv', punct=1)
 reader.readInput()
 data = reader.getData()
 tester = NewtonApproximation(RBF())
-kernel = tester.compute_kernel(data)
-picklefile = open('precomputedkernel.pkl', 'rw')
-pickle.dump(picklefile, kernel)
+kernel = {}
+for i in data:
+    id1 = i.getID()
+    kernel[id1] = {}
+    for j in data:
+        id2 = j.getID()
+        kernel[id1][id2] = tester.kernel.K(i.getFeature(), j.getFeature()) 
+picklefile = open('precomputedkernel.pkl', 'w')
+pickle.dump(kernel, picklefile)
 end = time.time()
 print('Kernel computed in ', (end-start)/60, ' seconds')
